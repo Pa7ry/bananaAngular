c9d3b9f616d1f0c31abbadd210c4ec0aa8c0baf5
Pa7ry <patriciadesantiagogallego@gmail.com>
2018-03-12 17:55:35 +0100

    video component

---
+++ bananaTube/src/app/video-edit/video-edit.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoEditComponent } from './video-edit.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VideoEditComponent]
+})
+export class VideoEditModule { }
